8609c3491cf74b46b722909ce931f5394282e146
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-23 14:06:07 -0800

    color of buttons changes

--- script.js
+++ script.js
@@ -3,5 +3,49 @@ const clickMeButton = document.querySelector(".click-me");
 
 function clickHandler(){
     alert(" u clicked me");
-  
-}  clickMeButton.addEventListener("click", clickHandler)
+    // clickMeButton.removeEventListener("click", clickHandler)
+}  clickMeButton.addEventListener("click", clickHandler,{once:true})
+   
+
+
+
+function backgroundchanger(){
+    // document.body.style.backgroundColor = "pink";
+    document.body.classList.add("pink-bg")
+}
+clickMeButton.addEventListener("click",backgroundchanger)
+
+function changetext(){
+    if(clickMeButton.textContent===" click me!")
+    {
+        clickMeButton.textContent="clicked";
+    }
+    else if(clickMeButton.textContent==="clicked")
+    {
+        clickMeButton.textContent=" click me!";
+    }
+    
+
+}
+clickMeButton.addEventListener("click",changetext);
+
+function updateImage(e){
+    console.log(e);
+    const image= document.querySelector("#shoppingcart")
+    image.src ='images/263142.png';
+    image.width=100;
+    image.height=100;
+    image.alt ="shopping cart icon"
+
+}
+clickMeButton.addEventListener("click",updateImage);
+
+const buttonContainer = document.querySelector(".button-container");
+
+function makegreen(e){
+    if (e.target.tagName==="BUTTON")
+    e.target.style.backgroundColor="green";
+    
+
+}
+buttonContainer.addEventListener("mouseover",makegreen);
