987fc97680210fcc5f8633f879af5551a4744653
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-23 12:33:05 -0800

    first button func

---
+++ script.js
@@ -0,0 +1,7 @@
+const clickMeButton = document.querySelector(".click-me");
+// console.log(clickMeButton);
+
+function clickHandler(){
+    alert(" u clicked me");
+  
+}  clickMeButton.addEventListener("click", clickHandler)
